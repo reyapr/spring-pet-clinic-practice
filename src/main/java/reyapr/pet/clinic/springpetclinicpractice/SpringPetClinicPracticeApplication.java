4d698f880a439a88011ff94bf800d05b060bb3c9
Renal Apriansyah <>
2020-02-13 07:08:48 +0700

    initiate project

---
+++ src/main/java/reyapr/pet/clinic/springpetclinicpractice/SpringPetClinicPracticeApplication.java
@@ -0,0 +1,13 @@
+package reyapr.pet.clinic.springpetclinicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reyapr/pet/clinic/springpetclinicpractice/SpringPetClinicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package reyapr.pet.clinic.springpetclinicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
